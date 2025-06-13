8ddc9e751fb0a45fb9a916c1098a64c7ab34d292
LoboMarketero <marketerolobo@gmail.com>
2025-06-13 01:32:13 -0300

    Fix video with correct vertical aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,8 @@ function App() {
       <div className="bg-white/30">
         <div className="container-custom relative z-10">
           <div className="flex flex-col items-center">
-            <div className="w-full max-w-4xl mb-4">
-              <div style={{position:'relative',paddingTop:'56.25%'}}>
+            <div className="w-full max-w-md mb-4">
+              <div style={{position:'relative',paddingTop:'176.66666666666666%'}}>
                 <iframe 
                   id="panda-3cccc2a2-419f-40a7-a19e-976b4b3b04a2" 
                   src="https://player-vz-ff1d2603-87c.tv.pandavideo.com.br/embed/?v=3cccc2a2-419f-40a7-a19e-976b4b3b04a2" 
